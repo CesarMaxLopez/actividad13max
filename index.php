4d1ab5704bcdabe0c19c7d09168fdeabb24f9533
CesarMaxLopez <160643427+CesarMaxLopez@users.noreply.github.com>
2024-06-14 01:43:45 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+  <head>
+    <title>Max bot</title>
+  </head>
+  <body>
+    <?php echo'Hola mi bro<br>'?>
+    <?php
+    $x = 7;
+    $y = 5;
+
+    $resultado_suma = $x + $y;
+    $resultado_resta = $x - $y;
+    $resultado_multiplicacion = $x * $y;
+    $resultado_division = $x / $y;
+
+    echo "El resultado de la suma es:   $resultado_suma<br>";
+    echo "La resta de $x - $y es:  $resultado_resta<br>";
+    echo "La multi de $x * $y es : $resultado_multiplicacion<br>";
+    echo "El resultado de la division es : $resultado_division<br>";
+    ?>
+  </body>
+</html>
